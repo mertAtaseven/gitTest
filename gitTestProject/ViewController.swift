a869f34073d4b9675af8593632c10279412991bc
Mert Ataseven <ataseven.mert@gmail.com>
2021-12-15 19:44:29 +0300

    first commit

---
+++ gitTestProject/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  gitTestProject
+//
+//  Created by Mert Ataseven on 15.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("git")
+        print("git2")
+        
+        
+        
+    }
+
+
+}
+
